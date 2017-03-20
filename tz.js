e5c72c193aa9582fb55047ad7f159cf406c8ea79
James Adams <jamesadams94@gmail.com>
2017-03-20 18:15:30 +1100

    ideas

---
+++ tz.js
@@ -0,0 +1,31 @@
+//Incoming : city string
+var city_string = "";
+const tz = require('tzlookup').tzNameAt;
+//https://maps.googleapis.com/maps/api/geocode/json?address=Melbourne&key=AIzaSyDwhoEot-eaB2-QR9WQu8OGV1rU-7fKzpc
+
+//Cool we have lat long:
+timezone = tz(lat,long);
+
+//Cool we have the timezone:
+
+
+//https://github.com/TooTallNate/node-time
+
+//Use this for the timezone -> time stage.
+
+//Cool, now we have time in the given city.
+
+//If between 7am-11pm - "Yeah sure, call your friend. Don't talk about the weather."
+
+//Questions:
+
+//Will this be fast enough?
+
+//Adjustable times.
+
+//Is your friend nocturnal?
+
+//Who's your friend? Shoot em an email so they can check when to call you?
+
+//Location look up based on facebook/twitter integration. Show a list of friends who should be awake.
+
